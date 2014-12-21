59584fc5040db0f851a6d84ca015db0ade46900e
Eren Sezener <erensezener@gmail.com>
2014-12-21 16:34:59 +0200

    Initial Commit

---
+++ Bayesian_Log/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bayesian_Log
+//
+//  Created by Eren Sezener on 21/12/14.
+//  Copyright (c) 2014 Eren Sezener. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
